044968e3981099264cfc325c60b23784529b5b9f
Ajao-Abeeb <abeebajao526@gmail.com>
2023-03-08 17:45:14 +0100

    code continue today

--- js/custom.js
+++ js/custom.js
@@ -94,3 +94,21 @@ $('.owl-features').owlCarousel({
 
 
 })
+// let backtotop = select('.back-to-top')
+// if (backtotop) {
+//   const toggleBacktotop = () => {
+//     if (window.scrollY > 100) {
+//       backtotop.classList.add('active')
+//     } else {
+//       backtotop.classList.remove('active')
+//     }
+//   }
+//   window.addEventListener('load', toggleBacktotop)
+//   onscroll(document, toggleBacktotop)
+// }
+
+ //    * Preloader
+ let loader = document.getElementById('preloader')
+setTimeout(()=>{
+    loader.remove();
+}, 2000); 
